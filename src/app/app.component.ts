1f99dd59996f59f8e64a5d362224a69c2bddd386
akhilkm2590 <akhilkm25@gmail.com>
2023-04-06 11:38:24 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  
+  title = 'dashboard';
+  isMenuCollapsed: boolean = true;
+  private data = [
+    {"Framework": "Vue", "Stars": "166443"},
+    {"Framework": "React", "Stars": "150793"},
+    {"Framework": "Angular", "Stars": "62342"},
+    {"Framework": "Backbone", "Stars": "27647"},
+    {"Framework": "Ember", "Stars": "21471"},
+  ];
+  private svg: any;
+  private margin = 50;
+  private width = 750 - (this.margin * 2);
+  private height = 400 - (this.margin * 2);
+  
+
+  ngOnInit() {
+    this.createSvg();
+    this.drawBars(this.data);
+  }
+
+  menuExpandOrCollapse() {
+    this.isMenuCollapsed = !this.isMenuCollapsed;
+  }
+
+  loadPage() {
+    setTimeout(()=>{       
+      window.location.reload();  
+      this.isMenuCollapsed = true;
+    }, 500);
+  }
+
+  createSvg(): void {
+    this.svg = d3.select("figure#bar")
+    .append("svg")
+    .attr("width", this.width + (this.margin * 2))
+    .attr("height", this.height + (this.margin * 2))
+    .append("g")
+    .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
+  }
+
+  drawBars(data: any[]): void {
+    // Create the X-axis band scale
+    const x = d3.scaleBand()
+    .range([0, this.width])
+    .domain(data.map(d => d.Framework))
+    .padding(0.2);
+  
+    // Draw the X-axis on the DOM
+    this.svg.append("g")
+    .attr("transform", "translate(0," + this.height + ")")
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+    .attr("transform", "translate(-10,0)rotate(-45)")
+    .style("text-anchor", "end");
+  
+    // Create the Y-axis band scale
+    const y = d3.scaleLinear()
+    .domain([0, 200000])
+    .range([this.height, 0]);
+  
+    // Draw the Y-axis on the DOM
+    this.svg.append("g")
+    .call(d3.axisLeft(y));
+  
+    // Create and fill the bars
+    this.svg.selectAll("bars")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", (d: any) => x(d.Framework))
+    .attr("y", (d: any) => y(d.Stars))
+    .attr("width", x.bandwidth())
+    .attr("height", (d: any) => this.height - y(d.Stars))
+    .attr("fill", "#d04a35");
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BarComponent } from './bar/bar.component';
+import { PieComponent } from './pie/pie.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BarComponent,
+    PieComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
